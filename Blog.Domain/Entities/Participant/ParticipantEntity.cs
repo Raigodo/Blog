2f05503c089cd219122d47891c966837905b6680
Valters <103326282+Raigodo@users.noreply.github.com>
2023-11-25 16:29:18 +0200

    Revert "Adding database"

--- Blog.Domain/Entities/Participant/ParticipantEntity.cs
+++ Blog.Domain/Entities/Participant/ParticipantEntity.cs
@@ -1,37 +1,25 @@
-﻿using Blog.Domain.Base;
-using Blog.Domain.Entities.Post;
-using Blog.Domain.Entities.Post.Vo;
-using Blog.Domain.Entities.User;
+﻿using Blog.Domain.Entities.Post.Vo;
 using Blog.Domain.Entities.User.Vo;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Runtime.Serialization;
 
 namespace Blog.Domain.Entities.Participant;
 
-public sealed class ParticipantEntity : BaseEntity<PostId, UserId>
+public sealed class ParticipantEntity
 {
-    public ParticipantEntity() 
-        : base(
-            new PostId(Guid.Empty),
-            new UserId(Guid.Empty)) { }
-
-    private ParticipantEntity(PostId postId, UserId userId) : base(postId, userId) { }
-
-    //public PostId PostId => this.LeftKey;  //BaseEntity protected property
-    //public UserId UserId => this.RightKey; //BaseEntity protected property
+    private ParticipantEntity(UserId userId, PostId postId)
+    {
+        UserId = userId;
+        PostId = postId;
+    }
+    public UserId UserId { get; private init; }
+    public PostId PostId { get; private init; }
     public bool ReceiveNotifications { get; set; }
-
-    //nullable to supress warnings about null to not-null conversion
-    public PostEntity? Post { get; private set; }
-    public UserEntity? User { get; private set; }
 
     public static ParticipantEntity Create(UserId userId, PostId postId)
     {
-        var participant = new ParticipantEntity(postId, userId)
+        var participant = new ParticipantEntity(userId, postId)
         {
             ReceiveNotifications = false,
         };
-
         return participant;
     }
 }
